18b5f847873ed49dd107c3e71d04eaf466bb5203
yeafel@qq.com <kyf19960629>
2019-09-11 15:32:16 +0800

    项目初始化

---
+++ src/main/java/www/elastic/co/elasticsearch/App.java
@@ -0,0 +1,13 @@
+package www.elastic.co.elasticsearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

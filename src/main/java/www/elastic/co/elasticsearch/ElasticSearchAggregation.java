f6bbc03e196bf73abfcb3fab261db32c27f838a7
yeafel@qq.com <kyf19960629>
2019-09-12 14:37:15 +0800

    通过路由极速查询

--- src/main/java/www/elastic/co/elasticsearch/ElasticSearchAggregation.java
+++ src/main/java/www/elastic/co/elasticsearch/ElasticSearchAggregation.java
@@ -1,12 +1,18 @@
 package www.elastic.co.elasticsearch;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.aggregations.Aggregation;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
@@ -18,6 +24,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Yeafel
@@ -113,6 +121,109 @@ public class ElasticSearchAggregation {
             Sum sum = bucket.getAggregations().get("by_score");
             System.out.println(bucket.getKey()+":"+sum.getValue());
         }
+        client.close();
+
+    }
+
+
+    /**
+     * 支持多索引、多类型查询
+     */
+    @Test
+    public void test3() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery())
+                .from(0)
+                .size(100);
+
+        //searchRequest.indices("test");
+        //多索引查询，多索引支持通配符
+        //searchRequest.indices("test", "test2");
+        searchRequest.indices("test*");
+
+        //searchRequest.types("user");
+        //多类型查询，多类型不支持通配符
+        searchRequest.types("test", "class");
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] hits2 = hits.getHits();
+        for (SearchHit documentFields : hits2) {
+            System.out.println(documentFields.getSourceAsString());
+        }
+
+        client.close();
+
+    }
+
+
+    /**
+     * 极速查询插入测试数据
+     */
+    @Test
+    public void test4() throws IOException {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("phone", "13602456782");
+        jsonMap.put("name", "tom");
+        jsonMap.put("sex", "male");
+        jsonMap.put("age", "16");
+
+        Map<String, Object> jsonMap2 = new HashMap<>();
+        jsonMap2.put("phone", "18912453678");
+        jsonMap2.put("name", "john");
+        jsonMap2.put("sex", "male");
+        jsonMap.put("age", "28");
+
+        Map<String, Object> jsonMap3 = new HashMap<>();
+        jsonMap3.put("phone", "15212453678");
+        jsonMap3.put("name", "lily");
+        jsonMap3.put("sex", "female");
+        jsonMap3.put("age", "18");
+
+        BulkRequest request = new BulkRequest();
+        request.add(new IndexRequest("test8", "user").routing(jsonMap.get("phone").toString().substring(0, 3)).source(jsonMap))
+                .add(new IndexRequest("test8", "user").routing(jsonMap2.get("phone").toString().substring(0, 3)).source(jsonMap2))
+                .add(new IndexRequest("test8", "user").routing(jsonMap3.get("phone").toString().substring(0, 3)).source(jsonMap3));
+
+        BulkResponse bulkResponse = client.bulk(request, RequestOptions.DEFAULT);
+        if (bulkResponse.hasFailures()) {
+            for (BulkItemResponse bulkItemResponse : bulkResponse) {
+                if (bulkItemResponse.isFailed()) {
+                    BulkItemResponse.Failure failure = bulkItemResponse.getFailure();
+                    System.out.println(failure.getMessage());
+                }
+            }
+        }
+
+
+    }
+
+
+    /**
+     * 通过路由极速查询
+     */
+    @Test
+    public void test5() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery())
+                .from(0)
+                .size(10);
+
+        searchRequest.indices("test8");
+        searchRequest.types("user");
+        searchRequest.source(searchSourceBuilder);
+        searchRequest.routing("18912453678".substring(0, 3));
+
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] hits2 = hits.getHits();
+        for (SearchHit documentFields : hits2) {
+            System.out.println(documentFields.getSourceAsString());
+        }
+
         client.close();
 
     }
